1b5961fb7fb40ca03ec74593c6c174add29debe5
ninja-1337 <evangelou.e.95@gmail.com>
2023-01-06 05:59:11 +0200

    init

--- src/pages/globe.js
+++ src/pages/globe.js
@@ -1,7 +1,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-
+import countries from "./../../data.json";
 const ReactGlobe = dynamic(import("react-globe.gl"), { ssr: false });
 
 function Globe() {
@@ -9,27 +9,25 @@ function Globe() {
   //const [generatedData, setGeneratedData] = useState([]);
 
   useEffect(() => {
-    const N = 20;
-    const gData = [...Array(N).keys()].map(() => ({
-      startLat: (Math.random() - 0.5) * 180,
-      startLng: (Math.random() - 0.5) * 360,
-      endLat: (Math.random() - 0.5) * 180,
-      endLng: (Math.random() - 0.5) * 360,
-      color: [
-        ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
-        ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
-      ],
-    }));
-
     //setGeneratedData(gData);
-    setArcsData(gData);
+    setArcsData(countries.features);
   }, []);
 
   return (
     <ReactGlobe
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
-      showGraticules={true}
-      hexPolygonsData={""}
+      hexPolygonsData={arcsData}
+      hexPolygonResolution={2}
+      hexPolygonMargin={0.05}
+      hexPolygonColor={() =>
+        `#${Math.round(Math.random() * Math.pow(2, 18))
+          .toString(16)
+          .padStart(6, "0")}`
+      }
+      hexPolygonLabel={({ properties: d }) => `
+      <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+      Population: <i>${d.POP_EST}</i>
+    `}
     />
   );
 }
