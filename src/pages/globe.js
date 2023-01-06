ccb268b28e969174eb8ed24d605841d6095d7006
ninja-1337 <evangelou.e.95@gmail.com>
2023-01-06 20:35:32 +0200

    styling

--- src/pages/globe.js
+++ src/pages/globe.js
@@ -14,24 +14,31 @@ function Globe() {
   }, []);
 
   return (
-    <div className="h-1/2 w-1/2">
-      <ReactGlobe
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
-        hexPolygonsData={arcsData}
-        hexPolygonResolution={2}
-        hexPolygonMargin={0.05}
-        hexPolygonColor={() =>
-          `#${Math.round(Math.random() * Math.pow(2, 18))
-            .toString(16)
-            .padStart(6, "0")}`
-        }
-        hexPolygonLabel={({ properties: d }) => `
+    <>
+      <Head>
+        <title>PayTrends</title>
+        <meta name="description" content="Paytrends" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-1/2 w-1/2">
+        <ReactGlobe
+          globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+          hexPolygonsData={arcsData}
+          hexPolygonResolution={2}
+          hexPolygonMargin={0.05}
+          hexPolygonColor={() =>
+            `#${Math.round(Math.random() * Math.pow(2, 18))
+              .toString(16)
+              .padStart(6, "0")}`
+          }
+          hexPolygonLabel={({ properties: d }) => `
       <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
       Population: <i>${d.POP_EST}</i>
     `}
-      />
-      <div>Hello</div>
-    </div>
+        />
+        <div>Hello</div>
+      </div>
+    </>
   );
 }
 
