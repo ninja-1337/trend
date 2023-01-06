b5e22171b59e3838289e7b8855405f6e48238dec
ninja-1337 <evangelou.e.95@gmail.com>
2023-01-06 20:47:22 +0200

    head

--- src/pages/globe.js
+++ src/pages/globe.js
@@ -2,7 +2,7 @@ import React from "react";
 import dynamic from "next/dynamic";
 import { useLayoutEffect, useState } from "react";
 import countries from "./../../data.json";
-const ReactGlobe = dynamic(import("react-globe.gl"), { ssr: true });
+const ReactGlobe = dynamic(import("react-globe.gl"));
 import Head from "next/head";
 
 function Globe() {
